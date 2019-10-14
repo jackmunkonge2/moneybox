583318f55daab99320b1cf49cea04f7ac3dacd8d
Jack Munkonge <jackmunkonge@gmail.com>
2019-10-14 18:41:49 +0100

    Initial Commit

---
+++ Moneybox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Moneybox
+//
+//  Created by Jack Munkonge on 14/10/2019.
+//  Copyright © 2019 Organisation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
